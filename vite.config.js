8e65699043c0d99d31fb2f61b92e6c1b8b3900e3
ikwerre-dev <investorhonour@gmail.com>
2025-02-28 00:05:32 +0100

    proposing a fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,33 @@
 import { defineConfig } from "vite";
 import Sitemap from "vite-plugin-sitemap";
 import react from "@vitejs/plugin-react";
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     Sitemap({
       hostname: "https://robinsonhonour.me",
       dynamicRoutes: ["/"],
+      outDir: './dist',
+      robots: {
+        policies: [
+          {
+            userAgent: '*',
+            disallow: ['/private/']
+          }
+        ],
+        sitemap: 'https://robinsonhonour.me/sitemap.xml'
+      }
     }),
   ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  }
 });
