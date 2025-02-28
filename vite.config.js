06b9ebdf98c19361dece13eaccb00264bf5f127d
ikwerre-dev <investorhonour@gmail.com>
2025-02-28 06:46:20 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from "vite";
-import Sitemap from "vite-plugin-sitemap";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
     react(),
-    Sitemap({
-      hostname: "https://my-portfolio-git-main-ikwerredevs-projects.vercel.app",
-      dynamicRoutes: ["/"],
-      outDir: './dist',
-      robots: false,
-      exclude: [], // Add empty exclude array
-      urls: ["/"] // Add explicit URLs array
-    }),
+
   ],
 });
