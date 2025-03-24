0470dca7d46b49158612ddbfd34b1834a4ed665f
ikwerre-dev <investorhonour@gmail.com>
2025-03-24 12:00:00 +0100

    feat: Portfolio website launch - March 24th 2025

--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -33,7 +33,7 @@ const Resume = ({ pdfUrl, fileName, buttonText }: ResumeProps) => {
     <div className="transition-all duration-300 active:scale-[0.96] max-w-[200px] mx-auto md:mx-0 flex justify-center lg:flex lg:justify-start">
       <span
         onClick={downloadPDF}
-        className="cursor-pointer bg-gradient-to-r max-w-[197px] lg:max-w-[252px] to-[#D9D9D9] from-[#D9D9D900] py-0.5 pr-0.5" >
+        className="cursor-pointer bg-gradient-to-r max-w-[197px] lg:max-w-[252px] to-[#D9D9D9] from-[#D9D9D900] py-0.5 pr-0.5">
         <p className="bg-[#0b040c] px-[17px] py-[18px] font-medium text-[18px] font-clash">
           {buttonText || "Download PDF"}
         </p>
